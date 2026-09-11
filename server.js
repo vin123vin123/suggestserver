d9a8806c49d5235c91b151effcd54197aa8d1476
vin123vin123 <vin33666@gmail.com>
2026-09-11 18:11:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -234,6 +234,10 @@ app.get('/admin', (req, res) => {
     </html>
   `);
 });
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server executing securely on port ${PORT}`);
+});
 
 
 
